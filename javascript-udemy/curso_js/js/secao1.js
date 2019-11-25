466db0757a2ac8e1c81e3aa769852099f35ac8c5
System Administrator <root@Mac-Pro-de-e-ig-1947.local>
2019-11-25 10:23:58 -0300

    espelhando repositorios

---
+++ javascript-udemy/curso_js/js/secao1.js
@@ -0,0 +1,50 @@
+//aula 1
+// Primeiros Passos
+console.log("Teste");
+alert("Teste");
+console.log("//////////////////////////////");
+
+//aula 2
+// Sintaxe
+console.log("//////////////////////////////");
+//aula 3
+//Variáveis
+var latitude = 40.789456;
+var longetude = -8.654;
+
+console.log("//////////////////////////////");
+//aula 4
+//Tipos de Dados - Strings
+var nome = "João";
+var sobrenome = "Sobrenome";
+var cpf = '123.456.789-12'
+var telefone = '12345678'
+var ddd = '12'
+var email = 'email@email.com'
+var biografia = 'Lorem Upson Dolor.....'
+console.log('Nome Completo: ' + nome + '' + sobrenome);
+
+console.log("//////////////////////////////");
+//aula 5
+//Tipos de Dados - numbers
+
+
+console.log("//////////////////////////////");
+//aula 6
+//Tipos de Dados - Booleanos
+
+
+
+console.log("//////////////////////////////");
+//aula 7
+//Tipos de Dados - null e undefined
+
+
+console.log("//////////////////////////////");
+//aula 8
+//Exercicios 1 
+
+
+console.log("//////////////////////////////");
+//aula 9
+//Resolucao Exercicios
